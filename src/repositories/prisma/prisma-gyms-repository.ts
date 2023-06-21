303177e016a3398bd9c417a0f1ab58285d5c61f7
Petrus Pierre <petrus.tsx@gmail.com>
2023-06-21 17:45:26 -0300

    feat: create prisma repositories

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,48 @@
+import {FindManyNearbyParams, GymsRepository} from '@/repositories/gyms-repository';
+import {Gym, Prisma} from '@prisma/client';
+import {prisma} from '@/lib/prisma';
+
+export class PrismaGymsRepository implements GymsRepository {
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data
+    });
+
+    return gym;
+  }
+
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id
+      }
+    });
+
+    return gym;
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const maxDistance = 10;
+
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= ${maxDistance}
+    `;
+
+    return gyms;
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query
+        }
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    });
+
+    return gyms;
+  }
+}
