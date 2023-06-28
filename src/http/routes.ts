8760506bb08c94260875776b4c4ef0d3047dd188
Petrus Pierre <petrus.tsx@gmail.com>
2023-06-28 17:06:47 -0300

    feat: add profile endpoint data

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,10 +2,13 @@ import { FastifyInstance } from 'fastify';
 import { register } from './controllers/register';
 import {authenticate} from '@/http/controllers/authenticate';
 import {profile} from '@/http/controllers/profile';
+import {verifyJWT} from '@/http/middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/session', authenticate);
 
-  app.get('/me', profile);
+  app.get('/me', {
+    onRequest: [verifyJWT]
+  }, profile);
 }
