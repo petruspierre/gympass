d68a8083e97cd7704069b42a775af20b614b4abd
Petrus Pierre <petrus.tsx@gmail.com>
2023-06-27 23:30:53 -0300

    feat: add missing factories for use cases

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import { register } from './controllers/register';
 import {authenticate} from '@/http/controllers/authenticate';
+import {profile} from '@/http/controllers/profile';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/session', authenticate);
+
+  app.get('/me', profile);
 }
