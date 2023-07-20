d2b8aaa73fcf249292e6bdead3ee9389ab1f90f6
Petrus Pierre <petrus.tsx@gmail.com>
2023-07-19 22:40:06 -0300

    feat: add role based access control

--- src/http/controllers/gyms/search.spec.ts
+++ src/http/controllers/gyms/search.spec.ts
@@ -13,7 +13,7 @@ describe('Search Gyms (E2E)', () => {
   });
 
   it('should be able to search gyms', async function () {
-    const { token } = await createAndAuthenticateUser(app);
+    const { token } = await createAndAuthenticateUser(app, true);
 
     await request(app.server)
       .post('/gyms')
